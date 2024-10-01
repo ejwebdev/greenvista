b3e48f121924b75f3dce0fb4a9ed8eca44e2f71a
ejwebdev <ejwebdev25@gmail.com>
2024-10-01 11:34:16 +0800

    Tailwind and React

--- src/components/footer-page/Footer.jsx
+++ src/components/footer-page/Footer.jsx
@@ -1,136 +1,90 @@
-import { Link } from "react-router-dom";
 import "./footer.css";
 
 const footerTopLinks = [
     {
         title: "Company",
-        link1: {
-            page: "details",
-            name: "Brand History",
-        },
-        link2: {
-            page: "details",
-            name: "Careers",
-        },
-        link3: {
-            page: "details",
-            name: "Testimonials",
-        },
+        links: [
+            { href: "", name: "Brand History" },
+            { href: "", name: "Careers" },
+            { href: "", name: "Testimonials" },
+        ],
     },
     {
         title: "Explore Homes",
-        link1: {
-            page: "details",
-            name: "Lifescapes",
-        },
-        link2: {
-            page: "details",
-            name: "House and Lot",
-        },
-        link3: {
-            page: "details",
-            name: "Condominium",
-        },
+        links: [
+            { href: "", name: "Lifescapes" },
+            { href: "", name: "House and Lot" },
+            { href: "", name: "Condominium" },
+        ],
     },
     {
         title: "Useful Links",
-        link1: {
-            href: "#aboutus",
-            name: "About Us",
-        },
-        link2: {
-            href: "#properties",
-            name: "Properties",
-        },
-        link3: {
-            href: "https://forms.gle/2qitqHSL12dKCgcBA",
-            target: "_blank",
-            name: "Seller's Guide",
-        },
+        links: [
+            { href: "#aboutus", name: "About Us" },
+            { href: "#properties", name: "Properties" },
+            {
+                href: "https://forms.gle/2qitqHSL12dKCgcBA",
+                target: "_blank",
+                name: "Seller's Guide",
+            },
+        ],
     },
     {
         title: "Social Media",
-        link1: {
-            href: "https://www.facebook.com/",
-            target: "_blank",
-            name: "Facebook",
-        },
-        link2: {
-            href: "https://www.tiktok.com/",
-            target: "_blank",
-            name: "Tiktok",
-        },
-        link3: {
-            href: "https://www.instagram.com/",
-            target: "_blank",
-            name: "Instagram",
-        },
+        links: [
+            {
+                href: "https://www.facebook.com/",
+                target: "_blank",
+                name: "Facebook",
+            },
+            {
+                href: "https://www.tiktok.com/",
+                target: "_blank",
+                name: "Tiktok",
+            },
+            {
+                href: "https://www.instagram.com/",
+                target: "_blank",
+                name: "Instagram",
+            },
+        ],
     },
 ];
 
 function Footer() {
+    // Smooth scroll
+    const smoothScroll = (event, href) => {
+        if (href.startsWith("#")) {
+            event.preventDefault();
+            const targetElement = document.getElementById(href.substring(1));
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: "smooth" });
+            }
+        }
+    };
+
     return (
         <section className="footer">
             <div className="footer-cntnr">
                 <div className="footer-top">
-                    {footerTopLinks.map((footerTopLinks, index) => (
+                    {footerTopLinks.map((section, index) => (
                         <div key={index}>
-                            <h3>{footerTopLinks.title}</h3>
+                            <h3>{section.title}</h3>
                             <ul>
-                                {footerTopLinks.link1.href && (
-                                    <li>
+                                {section.links.map((link, linkIndex) => (
+                                    <li key={linkIndex}>
                                         <a
-                                            href={footerTopLinks.link1.href}
-                                            target={footerTopLinks.link1.target}
+                                            href={link.href}
+                                            target={link.target || "_self"}
                                             rel="noopener noreferrer"
+                                            onClick={(e) =>
+                                                smoothScroll(e, link.href)
+                                            }
                                         >
-                                            {footerTopLinks.link1.name}
+                                            {link.name}
                                         </a>
                                     </li>
-                                )}
-                                {footerTopLinks.link2.href && (
-                                    <li>
-                                        <a
-                                            href={footerTopLinks.link2.href}
-                                            target={footerTopLinks.link2.target}
-                                            rel="noopener noreferrer"
-                                        >
-                                            {footerTopLinks.link2.name}
-                                        </a>
-                                    </li>
-                                )}
-                                {footerTopLinks.link3.href && (
-                                    <li>
-                                        <a
-                                            href={footerTopLinks.link3.href}
-                                            target={footerTopLinks.link3.target}
-                                            rel="noopener noreferrer"
-                                        >
-                                            {footerTopLinks.link3.name}
-                                        </a>
-                                    </li>
-                                )}
-                                {footerTopLinks.link1.page && (
-                                    <li>
-                                        <Link to={footerTopLinks.link1.page}>
-                                            {footerTopLinks.link1.name}
-                                        </Link>
-                                    </li>
-                                )}
-                                {footerTopLinks.link2.page && (
-                                    <li>
-                                        <Link to={footerTopLinks.link2.page}>
-                                            {footerTopLinks.link2.name}
-                                        </Link>
-                                    </li>
-                                )}
-                                {footerTopLinks.link3.page && (
-                                    <li>
-                                        <Link to={footerTopLinks.link3.page}>
-                                            {footerTopLinks.link3.name}
-                                        </Link>
-                                    </li>
-                                )}
+                                ))}
                             </ul>
                         </div>
                     ))}
@@ -140,10 +94,10 @@ function Footer() {
                     <p>&copy; 2024 Enrico Jay Sison</p>
                     <ul>
                         <li>
-                            <Link to="details">Privacy Policy</Link>
+                            <a href="">Privacy Policy</a>
                         </li>
                         <li>
-                            <Link to="details">Terms of Service</Link>
+                            <a href="">Terms of Service</a>
                         </li>
                     </ul>
                 </div>
